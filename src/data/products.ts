c4a02ef498d19089af0d5b22e30ce3e28d24ef64
Mohammad Altamimi <44396391+mohammedkhal@users.noreply.github.com>
2026-07-24 18:08:24 +0300

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -71,7 +71,7 @@ export const products: Product[] = [
     name: 'Acoustic Slat Architectural Wall Panel',
     sku: 'EOS-AP-8830',
     price: '$120.00',
-    arPlacement: true
+    arPlacement: true,
     priceValue: 120.0,
     category: 'Wall Systems & Acoustics',
     description:
